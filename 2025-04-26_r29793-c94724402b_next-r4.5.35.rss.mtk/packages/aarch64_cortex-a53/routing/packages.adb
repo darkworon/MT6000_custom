44d96cb02f206293b5af39db6a6a633cc579fd3f
Oleg Mikhailov <woron@nights.su>
2025-04-26 20:41:18 +0000

    new build 2025-04-26_r29793-c94724402b_next-r4.5.35.rss.mtk

---
+++ 2025-04-26_r29793-c94724402b_next-r4.5.35.rss.mtk/packages/aarch64_cortex-a53/routing/packages.adb
@@ -0,0 +1,5 @@
+ADBdUR]HTAž]íÇŒÚb#¨À-™÷îº¹&¦¦†°F¤Q`R³wfwoÝÙîÌ½«`%õ RD	Tô…=HÑ=”Ö‹Ò‹oùP=DPA?/ÝÍ¨ïž™3ç;ß7s§­£Ý´œ’Òx$«RT[Ú1OSÏÑUñX<aÖn}x¸§eëþÓ¯†#Ãcg/G*BGÏüšn
+”ËÁV@!']¶Ðž­mŸCŽ
+šÉq¡ÁÓ¶cë!HKÚÍ6³ÏìÁï^Óhc>g&tëRœ*›»ã:+Y‰aI‘¶3žk‹ ¶5(ó±–AÞ•¾Í¸%s(c¨,u@Ké¨›ñ”—É¹TA;Ž	}Y”ÊSë8ÍpHy¶ÃøÜUH…òyA¦Ag9x
+­(,å‹g0¡W£2u(/eÉÚaê?Ÿ´è¡dO
+Çü_xA\Ð”ƒG^K©keŽXÒÕ|Ð ‰íá=û’FÜŒH‚îÞÝÐÓÝNsÎTÔ•è@d¢åßÖkçPä 'wÛÊB³*´ªb²ð7g2Þ²!«u^5E£…BÁ”y.ŒWYSº™h¥c§¬â¯ÿ #Õ¸ŽCõ¥ç$#K0çêâ”0®+1~…•øN6!––½”öp¿›1Ÿ–YþÃÚÕ'W¾7ö,8vþÄëºycö@MWÂ¿Ó„ukË	YH"·‘.úEL &ó²8bÁPrŽ„„„"¨Ñ(êå1Vü)™Ây1ÒJ*gØƒðõ‘:‘.ÔvNÜNÇ:ƒP¼:÷è†U½nôýxÝÌ@òeCãÝo»n¾ýr)k7“ç??Ô¾ëšß!ÿÜhû<µeçÂ©'Á“?î¿¹våøØÿ7
